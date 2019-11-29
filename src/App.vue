2de21e58b8fbe8c61a065d803377f2e50b0c118a
Jerome Doran <disneyfan9184@gmail.com>
2019-11-29 16:01:20 -0500

    Finished events

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- Need to have everything written inside one root element -->
+  <div>
+    <app-header
+      v-bind:title="title"
+      v-on:changeTitle="updateTitle($event)"
+    ></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+  components: {
+    // Register components
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data() {
+    return {
+      ninjas: [
+        { name: 'Ryu', specialty: 'Vue Components', show: false },
+        { name: 'Yoshi', specialty: 'HTML Wizardry', show: false },
+        { name: 'Mario', specialty: 'Click Events', show: false },
+        { name: 'Luigi', specialty: 'Conditionals', show: false },
+        { name: 'Wario', specialty: 'Webpack', show: false },
+        { name: 'Bowser', specialty: 'Data Diggin', show: false }
+      ],
+      title: 'Vue Ninjas'
+    };
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
+  }
+};
+</script>
+
+// Need to add the scoped key word to style each component's tags separately
+<style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Need to have everything written inside one root element -->
+  <footer>
+    <p>{{ copyright }} {{ title }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      copyright: 'Copyright 2019'
+    };
+  }
+};
+</script>
+
+// Need to add the scoped key word to style each component's tags separately
+<style scoped>
+footer {
+  background: #222;
+  padding: 6px;
+}
+
+p {
+  color: lightgreen;
+  text-align: center;
+}
+</style>
