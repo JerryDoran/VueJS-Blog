ac7b01b2431298a3d21a3e8d618415ebbdf0bef0
Jerome Doran <disneyfan9184@gmail.com>
2019-12-01 11:57:32 -0500

    Finished basic blog form

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,32 @@
 <template>
   <!-- Need to have everything written inside one root element -->
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of the form</h3>
-        <p>Info about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="Name..." required />
-        <input type="password" placeholder="Password..." required />
-        <p>Info about the form</p>
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
-import formHelper from './components/formHelper.vue';
+// Import my components
+import addBlog from './components/addBlog.vue';
 
 export default {
   components: {
-    // Register components
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas,
-    'form-helper': formHelper
+    // Register my components
+    'add-blog': addBlog
   },
   data() {
-    return {
-      ninjas: [
-        { name: 'Ryu', specialty: 'Vue Components', show: false },
-        { name: 'Yoshi', specialty: 'HTML Wizardry', show: false },
-        { name: 'Mario', specialty: 'Click Events', show: false },
-        { name: 'Luigi', specialty: 'Conditionals', show: false },
-        { name: 'Wario', specialty: 'Webpack', show: false },
-        { name: 'Bowser', specialty: 'Data Diggin', show: false }
-      ],
-      title: 'Vue Ninjas',
-      slotTitle: 'I am a dynamic slot title'
-    };
+    return {};
   },
-  methods: {
-    updateTitle: function(updatedTitle) {
-      this.title = updatedTitle;
-    }
-  }
+  methods: {}
 };
 </script>
 
-// Need to add the scoped key word to style each component's tags separately
-<style scoped></style>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+body {
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- Need to have everything written inside one root element -->
+  <div id="add-blog">
+    <h2>Add a New Blog Post</h2>
+    <form action>
+      <label>Blog Title:</label>
+      <input type="text" v-model.lazy="blog.title" required />
+      <label>Blog Content:</label>
+      <textarea rows="10" v-model.lazy="blog.content"></textarea>
+    </form>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{ blog.title }}</p>
+      <p class="light-text">
+        <span class="normal-text">Content:</span>
+      </p>
+      <p>{{ blog.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blog: {
+        title: '',
+        content: ''
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+
+
+<style>
+#add-blog * {
+  box-sizing: border-box;
+}
+
+#add-blog {
+  width: 80%;
+  margin: 20px auto;
+  max-width: 650px;
+}
+
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+
+input[type='text'],
+textarea {
+  padding: 5px;
+  border-radius: 5px;
+  outline: none;
+  display: block;
+  width: 100%;
+  font-size: 1rem;
+}
+
+input[type='text'] {
+  font-weight: 500;
+}
+
+textarea,
+.light-text {
+  font-weight: 300;
+}
+
+.normal-text {
+  font-weight: 400;
+}
+
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+
+h3 {
+  margin-top: 10px;
+}
+</style>
